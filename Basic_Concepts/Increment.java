19122d97054a5689d9d571a17f5e3c83422fcc6f
wahabu <ritscg2hibo@gmail.com>
2023-03-06 22:55:44 +0900

    Create new code file of Increment in Java

---
+++ Basic_Concepts/Increment.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Increment {
+  public static void main(String[] args) {
+    // taking initial score
+    int initSocre = scanner.nextInt();
+    int scoreTom = initScore;
+
+    System.out.println("Round 1 results:");
+    // fix
+    System.out.println(++scoreTom);
+  }
+}
