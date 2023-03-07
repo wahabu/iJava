33d72f7ff7b3aefd27db106c008c0238fc4b4cb1
wahabu <ritscg2hibo@gmail.com>
2023-03-07 23:31:06 +0900

    Create Java Decrement code file

---
+++ Basic_Concepts/Decrement.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Decrement {
+  public static void main(String[] args) {
+    // taking initial score
+    int initSocre = scanner.nextInt();
+    int scoreBob = initScore;
+
+    System.out.println("Round 1 results:");
+    // fix
+    System.out.println(--scoreBob);
+  }
+}
