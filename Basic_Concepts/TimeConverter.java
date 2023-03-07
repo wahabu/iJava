760ebc23d74097497bb4595514124762facc09aa
wahabu <ritscg2hibo@gmail.com>
2023-03-07 23:47:59 +0900

    Create Java code to convert days to seconds

---
+++ Basic_Concepts/TimeConverter.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TimeConverter {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int days = scanner.nextInt();
+
+    int hours, minutes, seconds;
+    hours = days * 24;
+    minutes = hours * 60;
+    seconds = minutes * 60;
+
+    System.out.println(seconds);
+  }
+}
