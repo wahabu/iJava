c4d33d68e44f763e18e44ab8a5ca35be39e293cd
wahabu <ritscg2hibo@gmail.com>
2023-03-09 23:57:38 +0900

    Create Conditionals and Loops folder and new file of conditional statement code

---
+++ ConditionalsAndLoops/ConditionalStatements.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ConditionalStatements {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    int price = scanner.nextInt();
+    // complete the code
+
+  }
+}
