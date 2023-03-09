1793eef14a3d7f00f90b4aef1eef98b482cd0fe9
wahabu <ritscg2hibo@gmail.com>
2023-03-09 23:49:57 +0900

    Edit Decrement.java file

--- Basic_Concepts/Decrement.java
+++ Basic_Concepts/Decrement.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Decrement {
   public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
     // taking initial score
     int initScore = scanner.nextInt();
     int scoreBob = initScore;
