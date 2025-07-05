8ffb5e0e44e17af9a9a16b2c52759cfe19f2e1c4
wahabu <ritscg2hibo@gmail.com>
2025-07-06 07:34:44 +0900

    Clean repo: only source files

---
+++ 0001/B.java
@@ -0,0 +1,5 @@
+public class B extends A {
+  public void test() {
+    System.out.println("B");
+  }
+}
---
+++ firstproject/src/main/java/com/mycompany/firstproject/Firstproject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.firstproject;
+
+/**
+ *
+ * @author WAHAB
+ */
+public class Firstproject {
+
+    public static void main(String[] args) {
+        // System is a class
+        // out is a static final PrintStream out
+        // print is a method inside in class System
+        System.out.println((5+1)/2);
+        System.out.print("Hello");
+    }
+}
