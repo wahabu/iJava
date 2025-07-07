54e67b345edafa99c123ce87f6b797829a6fc58b
wahabu <ritscg2hibo@gmail.com>
2025-07-08 05:03:28 +0900

    Cleaned and unified .gitignore rules

---
+++ SkillForge01/src/skillforge01/Main.java
@@ -0,0 +1,12 @@
+// This is my first testing class
+package skillforge01;
+
+/**
+ *
+ * @author WAHAB
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
