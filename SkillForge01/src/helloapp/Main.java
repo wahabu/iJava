501761356e5d004c1c01cb3e3d08278ae5f39df4
wahabu <ritscg2hibo@gmail.com>
2025-07-09 09:13:40 +0900

    refactor: move Main.java to vehicles package

--- SkillForge01/src/helloapp/Main.java
+++ SkillForge01/src/helloapp/Main.java
@@ -1,11 +1,8 @@
-// This is my first testing class
+
 package helloapp;
 
-/**
- *
- * @author WAHAB
- */
 public class Main {
+    //block
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
--- SkillForge02/src/vehicles/Main.java
+++ SkillForge02/src/vehicles/Main.java
@@ -1,15 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package helloapp;
+package vehicles; // Package name (like a folder for related classes)
 
 /**
- *
+ * Entry point class for testing or future expansion
  * @author WAHAB
  */
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class Main { // Class named Main - good for organizing the app's entry point
+    
+    public static void main(String[] args) { // Main method - Java starts from here when you run Main.java
+        System.out.println("Hello World!"); // Prints a greeting message to the console
     }
 }
