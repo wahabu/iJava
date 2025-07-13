03fbd5aef2a6a848000097a137491b22e868c8fd
wahabu <ritscg2hibo@gmail.com>
2025-07-13 18:24:06 +0900

    Add simple Java program to print person's name and age from arrays by index

---
+++ SkillForge-LearnJava/src/learn/PersonInfo.java
@@ -0,0 +1,33 @@
+package learn;
+
+public class PersonInfo {
+    public static void main(String[] args) {
+        String[] names = {
+            "Aさん",
+            "Bさん",
+            "Dさん",
+            "Jさん",
+            "Zさん"
+        };
+        
+        int[] ages = {
+            21,
+            22,
+            23,
+            24,
+            25
+        };
+        
+        int index = 4;
+        if (
+                index >= 0
+                        &&
+                index < ages.length
+                        && 
+                index < names.length
+        )   
+                        {
+                    System.out.println(names[index] + ": " + ages[index] + " Years");
+                 }
+    }
+}
