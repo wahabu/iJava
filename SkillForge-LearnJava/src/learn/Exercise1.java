0e1437c7debbfd81b298e932849fc1f85aa0956a
wahabu <ritscg2hibo@gmail.com>
2025-07-14 23:27:19 +0900

    Add a Java program to print 'Hello' on screen and your name on a seprate line.

---
+++ SkillForge-LearnJava/src/learn/Exercise1.java
@@ -0,0 +1,8 @@
+package learn;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        // Print a greeting message to the console
+        System.out.println("Hello\nWahab!");
+    }
+}
