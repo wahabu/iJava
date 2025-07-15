eb1a485883c65682adf88908e4a7cea6b93e8445
wahabu <ritscg2hibo@gmail.com>
2025-07-15 23:55:38 +0900

    Add a Java program to print the sum of two numbers.

---
+++ JavaExercisesBasics/src/basic_part_1/SumTwoNumbers.java
@@ -0,0 +1,11 @@
+// Write a Java program to print the sum of two number.
+
+package basic_part_1;
+
+public class SumTwoNumbers {
+
+    public static void main(String[] args) {
+            int a = 74, b = 36;
+            System.out.println(a + b);
+    }
+}
