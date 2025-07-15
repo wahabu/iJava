595751ee461e41a7c9dc163470b2d8aa8e617ec6
wahabu <ritscg2hibo@gmail.com>
2025-07-16 00:00:49 +0900

    Add a Java program to divide two numbers and print them on the screen.

---
+++ JavaExercisesBasics/src/basic_part_1/DivisionTowNumbers.java
@@ -0,0 +1,11 @@
+// Write a Java
+
+package basic_part_1;
+// DivisionTowNumbers
+public class DivisionTowNumbers {
+
+    public static void main(String[] args) {
+            int a = 50, b = 3;
+            System.out.println(a / b);
+    }
+}
