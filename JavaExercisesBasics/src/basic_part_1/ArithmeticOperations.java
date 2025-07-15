85912c360254a1a1b3702c3aa5d799c6a2c536e0
wahabu <ritscg2hibo@gmail.com>
2025-07-16 00:28:45 +0900

    Add a Java program to evaluate sample arithmetic expressions.

---
+++ JavaExercisesBasics/src/basic_part_1/ArithmeticOperations.java
@@ -0,0 +1,21 @@
+/* Write a Java program to print the results of the following operaions. 
+ * Test Data:
+ * a. -5 + 8 * 6 >> 43
+ * b. (55+9) % 9 >> 1
+ * c. 20 + -3*5 /8 >> 19
+ * d. 5 + 15 / 3 * 2 - 8 % 3 >> 13
+*/
+
+package basic_part_1;
+
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+    
+        int a = -5 + 8 * 6;
+        int b = (55+9) % 9;
+        int c = 20 + -3*5 / 8;
+        int d = 5 + 15 / 3 * 2 - 8 % 3;
+    
+        System.out.println(a + "\n" + b + "\n" + c + "\n" + d);
+    }
+}
