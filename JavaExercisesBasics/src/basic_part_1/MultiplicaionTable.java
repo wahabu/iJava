5b2dc56a790f19f6b601997ef66de330a9853a9c
wahabu <ritscg2hibo@gmail.com>
2025-07-18 23:59:36 +0900

    Add a Java program to Product of input number

---
+++ JavaExercisesBasics/src/basic_part_1/MultiplicaionTable.java
@@ -0,0 +1,23 @@
+/*
+
+ */
+package basic_part_1;
+
+import java.util.Scanner;
+
+public class MultiplicaionTable {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input from the user
+        Scanner in = new Scanner(System.in);
+   
+        // Prompt the user to input the first number
+        System.out.print("Input a number: ");
+        // Read and store the first number
+        int x = in.nextInt();
+        
+        // Calculate the product of the two numbers and display the result
+        System.out.println(x + " x " + 1 + " = " + x * 1);
+        System.out.println(x + " x " + 2 + " = " + x * 2);
+        System.out.println(x + " x " + 3 + " = " + x * 3);
+    }
+}
