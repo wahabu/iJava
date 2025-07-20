e39e8367275eba61ffff399e18c034dc6a87e767
wahabu <ritscg2hibo@gmail.com>
2025-07-20 22:23:56 +0900

    Update file contents with correct solution of the code

--- JavaExercisesBasics/src/basic_part_1/MultiplicaionTable.java
+++ JavaExercisesBasics/src/basic_part_1/MultiplicaionTable.java
@@ -1,6 +1,13 @@
-/*
-
+/* Write a Java program that takes a number as input and prints its multiplication table up to 10.
+ * Test Data:
+ * Input a number: 8
+ * Expected Output:
+ * 8 X 1 = 8
+ * 8 X 2 = 16
+ * ...
+ * 8 X 10 = 80
  */
+
 package basic_part_1;
 
 import java.util.Scanner;
@@ -10,16 +17,15 @@ public class MultiplicaionTable {
         // Create a Scanner object to read input from the user
         Scanner in = new Scanner(System.in);
    
-        // Prompt the user to input the first number
+        // Prompt the user to input a number
         System.out.print("Input a number: ");
-        // Read and store the first number
+        
+        // Read and store the input number
         int x = in.nextInt();
         
         for (int i = 1; i <=10; i++) {
-        // Calculate the product of the two numbers and display the result
+            // Calculate and print the result of x multiplied by (i+1)
             System.out.println(x + " x " + i + " = " + x * (i));
-//        System.out.println(x + " x " + 2 + " = " + x * 2);
-//        System.out.println(x + " x " + 3 + " = " + x * 3);
         }
     }
 }
