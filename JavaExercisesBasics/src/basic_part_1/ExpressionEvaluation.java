e0a401eb617a9336bf79fab015c94ccb62dd9267
wahabu <ritscg2hibo@gmail.com>
2025-07-23 23:59:24 +0900

    Write a Java program to compute the specified expressions

---
+++ JavaExercisesBasics/src/basic_part_1/ExpressionEvaluation.java
@@ -0,0 +1,13 @@
+/*
+ * Write a Java program to compute the specified expressions and print the output. 
+ *
+*/
+package basic_part_1;
+
+/**
+ *
+ * @author WAHAB
+ */
+public class ExpressionEvaluation {
+    
+}
