0f622b3d6948192b759a2b64853be45180af9651
wahabu <ritscg2hibo@gmail.com>
2025-07-29 20:58:51 +0900

    Add a Java program to add two binary numbers.

---
+++ JavaExercisesBasics/src/basic_part_1/BinaryAddition.java
@@ -0,0 +1,17 @@
+/*
+ * Write a Java program to add two binary numbers.
+ * Input Data:
+ * input first binary number: 10
+ * input second binary number: 11
+ * Expected Output
+ * Sum of two binary numbers: 101
+ */
+package basic_part_1;
+
+/**
+ *
+ * @author WAHAB
+ */
+public class BinaryAddition {
+    
+}
