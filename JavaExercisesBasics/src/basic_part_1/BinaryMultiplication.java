fbd173479ba35e4ebc36e6e85c7aeb5be295c048
wahabu <ritscg2hibo@gmail.com>
2025-08-02 16:40:42 +0900

    Update file contents

--- JavaExercisesBasics/src/basic_part_1/BinaryMultiplication.java
+++ JavaExercisesBasics/src/basic_part_1/BinaryMultiplication.java
@@ -6,9 +6,21 @@
  * Expected Output
  * Product of two binary numbers: 110
  */
+
 package basic_part_1;
+import java.util.Scanner;
+
 
 
 public class BinaryMultiplication {
-    
+    public static void main(String[] args) {
+        // Declare variables to store two binary numbers and the product
+        long binary1, binary2, multiply = 0;
+        
+        // Initialize digit and factor variable for processing binary2
+        int digit, factor = 1;
+        
+        // Creater a Scanner object to read input from the user
+        Scanner in = new Scanner(System.in);
+    }
 }
