0d79c201f93665d2a0b88680691c7d8e60b6fc41
wahabu <ritscg2hibo@gmail.com>
2025-08-05 21:09:02 +0900

    Update file contents

--- JavaExercisesBasics/src/basic_part_1/BinaryMultiplication.java
+++ JavaExercisesBasics/src/basic_part_1/BinaryMultiplication.java
@@ -43,5 +43,34 @@ public class BinaryMultiplication {
             binary2 = binary2 / 10;
             factor = 10;
         }
+        
+        // Display the product of the two binary numbers
+        System.out.print("Product of two binary numbers: " + multiply + "\n");
     }
-}
+        
+        // Method to calculate the product of two binary numbers
+        static int binaryproduct(int binary1, int binary2) {
+            int i = 0, remainder = 0;
+            int [] sum = new int[20];
+            int binary_prod_result = 0;
+            
+            while (binary1 != 0 || binary2 != 0) {
+                sum[i++] = (binary1 % 10 + binary2 % 10 + remainder) % 2;
+                remainder = (binary1 % 10 + binary2 % 10 + remainder) / 2;
+                binary1 = binary1 / 10;
+                binary2 = binary2 / 10;
+            }
+            
+            if (remainder != 0) {
+                sum[i++] = remainder;
+            }
+            
+            --i;
+            
+            while (i >= 0) {
+                binary_prod_result = binary_prod_result * 10 + sum[i--];
+            }
+            
+            return binary_prod_result;
+        }
+    }
