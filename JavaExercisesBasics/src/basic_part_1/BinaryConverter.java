6437b282cfcd9a7013191f43c82032516675b09c
wahabu <ritscg2hibo@gmail.com>
2025-08-06 23:38:56 +0900

    Add a Java program to convert an integer number to a binary number

---
+++ JavaExercisesBasics/src/basic_part_1/BinaryConverter.java
@@ -0,0 +1,13 @@
+/*
+ * Write a Java program to multiply two binary numbers.
+ * Input Data:
+ * Input the first binary number: 10
+ * Input the second binary number: 11
+ * Expected Output
+ * Product of two binary numbers: 110
+ */
+package basic_part_1;
+
+public class BinaryConverter {
+    
+}
