a1762d7e53226980ca641a1dd94bb746eefda811
wahabu <ritscg2hibo@gmail.com>
2025-08-07 23:35:19 +0900

    Update file contents

--- JavaExercisesBasics/src/basic_part_1/BinaryConverter.java
+++ JavaExercisesBasics/src/basic_part_1/BinaryConverter.java
@@ -9,5 +9,9 @@
 package basic_part_1;
 
 public class BinaryConverter {
+    int dec_num, quot, i = 1, j;
+    int bin_num[] = new int[100];
     
+    // Create a Scanner object to read input from the user
+    Scanner scan = new Scanner(System.in);
 }
