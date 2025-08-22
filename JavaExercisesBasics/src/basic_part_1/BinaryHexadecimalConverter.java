a94b17dc58079ad3bababe36ce50b8c475490808
wahabu <ritscg2hibo@gmail.com>
2025-08-22 23:09:53 +0900

    Update file contents

--- JavaExercisesBasics/src/basic_part_1/BinaryHexadecimalConverter.java
+++ JavaExercisesBasics/src/basic_part_1/BinaryHexadecimalConverter.java
@@ -13,7 +13,7 @@ public class BinaryHexadecimalConverter {
     public static void main(String[] args) {
         // Declare an array to store hexadecimal digits, variables for calculation, and binary binary input
         int[] hex = new int[1000];
-        int i = 1, j = 0, rem, dec = 0, bin;
+        int i = 1, j, rem, dec = 0, bin;
         
         // Create a Scanner object to read input from the user
         Scanner in = new Scanner(System.in);
@@ -33,9 +33,20 @@ public class BinaryHexadecimalConverter {
         
         // Convert the decimal number to hexadecimal
         while (dec != 0) {
-            hex[i] = dec  16;
+            hex[i] = dec  % 16;
             dec = dec / 16;
             i++;
         }
+        
+        // Display the hexadecimal value
+        System.out.print("Hexadecimal value: ");
+        for (j = i - 1; j >= 0; j--) {
+            if (hex[j] > 9) {
+                System.out.print((char)(hex[j] + 55));
+            } else {
+                System.out.print(hex[j]);
+            }
+        }
+        System.out.print("\n");
     }
 }
