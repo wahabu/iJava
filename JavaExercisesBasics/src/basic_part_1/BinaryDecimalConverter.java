d13ae89d1ef3ad162c40e78e2288c58d99fe1856
wahabu <ritscg2hibo@gmail.com>
2025-08-24 20:52:34 +0900

    Add a Java program to convert a binary number to a decimal number

---
+++ JavaExercisesBasics/src/basic_part_1/BinaryDecimalConverter.java
@@ -0,0 +1,15 @@
+/*
+ * Write a Java program to convert a binary number to a decimal number.
+ * Input Data:
+ * Input a binary number: 100
+ * Expected Output
+ * 
+ * Decimal Number: 4
+ */
+package basic_part_1;
+
+/**
+ */
+public class BinaryDecimalConverter {
+    
+}
