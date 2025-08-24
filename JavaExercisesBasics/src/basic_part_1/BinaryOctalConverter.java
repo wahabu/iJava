6de2763ab962b9610732133af4332d4090e7ebc0
wahabu <ritscg2hibo@gmail.com>
2025-08-24 20:52:34 +0900

    Update file contents

--- JavaExercisesBasics/src/basic_part_1/BinaryOctalConverter.java
+++ JavaExercisesBasics/src/basic_part_1/BinaryOctalConverter.java
@@ -1,5 +1,10 @@
 /*
- * 
+ * Write a Java program to convert a decimal number to an octal number.
+ * Input Data:
+ * Input a Decimal Number: 15
+ * Expected Output
+ *
+ *Octal number is: 17
  */
 package basic_part_1;
 
