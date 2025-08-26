e754234f31fc82885b059608c28e4dd7a99cb386
wahabu <ritscg2hibo@gmail.com>
2025-08-26 14:00:00 +0900

    Update file contents

--- JavaExercisesBasics/src/basic_part_1/OctalDecimalConverter.java
+++ JavaExercisesBasics/src/basic_part_1/OctalDecimalConverter.java
@@ -1,5 +1,10 @@
 /*
  * Write a Java program to convert a octal number to a decimal number.
+ * Input Data:
+ * Input any octal number: 10
+ * Expected Output
+ *
+ * Equivalent decimal number: 8
  */
 package basic_part_1;
 
