11c48a4ab0c60310fc456887fa6295d11682caac
Wahab <52596957+wahabu@users.noreply.github.com>
2025-12-18 07:01:21 +0900

    Move first exercise to basics (E001)

--- JavaExercisesBasics/src/basics/E001_WelcomeApp.java
+++ JavaExercisesBasics/src/basics/E001_WelcomeApp.java
@@ -1,6 +1,6 @@
-package basic_part_1;
+package basics;
 
-public class WelcomeApp {
+public class E001_WelcomeApp {
     public static void main(String[] args) {
         // Print a greeting message to the console
         System.out.println("Hello\nWahab!");
