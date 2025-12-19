e49ebfb799b2477af8ad0bf7f188a7f3118f2916
Wahab <52596957+wahabu@users.noreply.github.com>
2025-12-19 12:44:21 +0900

    Add basic exercise E002: Sum of two numbers

---
+++ JavaExercisesBasics/src/basics/E002_SumTwoNumbers.java
@@ -0,0 +1,11 @@
+// Write a Java program to print the sum of two number.
+
+package basics;
+
+public class E002_SumTwoNumbers {
+
+    public static void main(String[] args) {
+        int a = 74, b = 36;
+        System.out.println(a + b);
+    }
+}
